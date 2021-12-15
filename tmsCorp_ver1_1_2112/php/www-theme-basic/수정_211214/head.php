4bd37def1e861929a84491237dc99efc963e001c
tms-jun <90189601+tms-jun@users.noreply.github.com>
2021-12-15 09:56:54 +0900

    ourServices - 각 페이지 링크 연결

---
+++ tmsCorp_ver1_1_2112/php/www-theme-basic/수정_211214/head.php
@@ -0,0 +1,90 @@
+<?php
+if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
+
+if (G5_IS_MOBILE) {
+    include_once(G5_THEME_MOBILE_PATH.'/head.php');
+    return;
+}
+
+include_once(G5_THEME_PATH.'/head.sub.php');
+include_once(G5_LIB_PATH.'/latest.lib.php');
+include_once(G5_LIB_PATH.'/outlogin.lib.php');
+include_once(G5_LIB_PATH.'/poll.lib.php');
+include_once(G5_LIB_PATH.'/visit.lib.php');
+include_once(G5_LIB_PATH.'/connect.lib.php');
+include_once(G5_LIB_PATH.'/popular.lib.php');
+?>
+
+<!-- 상단 시작 { -->
+<head>
+<meta charset="UTF-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<link rel="shortcut icon" href="./res/img/tms_favicon.ic.png" />
+<meta property="og:title" content="TMS Corporation" />
+<meta property="og:image" content="./res/img/tms_thumb.png" />
+<meta property="og:description" content="(주)티엠솔루션스 회사소개" />
+<meta property="og:url" content="www.tmscorp.co.kr" />
+<meta property="og:site_name" content="TMS Corporation">
+<meta property="og:type" content="website">
+<meta name="title" content="TMS Corporation">
+<meta name="description" content="(주)티엠솔루션스 회사소개">
+<meta name="keywords" content="티엠,티엠솔루션스,태양광,태양광에너지,태양광발전소,태양광컨설팅,신재생에너지,친환경에너지">
+<meta name="author" content="TMS Corporation">
+<meta name="naver-site-verification" content="c2da838378af2d1f9ea8664b85d78febc1e445d2" />
+<script src="https://use.fontawesome.com/releases/v5.2.0/js/all.js"></script>
+<script defer src="/res/js/tms_common.js"></script>
+<link rel="stylesheet" href="/res/css/tms_common.css">
+<link rel="stylesheet" href="/res/css/tms_responsive.css">
+<title>TMS Corporation</title>
+</head>
+
+<body id="ourStory">
+<button class="btn_arrow-up"><img src="/res/img/tms_ic_arrow_white.png" alt="next"></button>
+<header class="subpage">
+  <div class="header_inner">
+    <div class="navbar">
+      <div class="wrap_navbar_menu">
+        <a href="/index.html"><img class="navbar_logo" src="/res/img/tms_logo_white.svg" /></a>
+        <ul class="navbar_menu">
+          <li class="navbar_menu-item"><a href="/tmsolutions.html">티엠솔루션스</a></li>
+          <li class="navbar_menu-item"><a href="/ourServices.html">사업분야</a></li>
+          <li class="navbar_menu-item"><a href="/ourStory.html" class="selected">미디어</a></li>
+        </ul>
+        <!-- s: toggle button -->
+        <button class="navbar_toggle"><i class="fas fa-bars"></i></button>
+        <!-- e: toggle button -->
+      </div>
+    </div>
+    <div class="slogan">
+      <div class="slogan_inner">
+        <img src="/res/img/tms_ic_triangle.png">
+        <p><span class="slogan-bold">우리</span>의 이야기를</p>
+        <p>만들어 갑니다.</p>
+        <p class="slogan-bold fc-red">Our Story</p>
+      </div>
+    </div>
+  </div>
+  <div class="header_bg">
+    <div class="header_bg_inner">
+      <div class="header_bg_top"></div>
+      <img src="/res/img/tms_img_header.jpg" class="header_img">
+      <div class="header_bg_black"></div>
+    </div>
+    <div class="custom-shape-divider-bottom-1637742654">
+      <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+          <path d="M1200 120L0 16.48 0 0 1200 0 1200 120z" class="shape-fill"></path>
+      </svg>
+    </div>
+  </div>
+</header>
+<!-- } 상단 끝 -->
+
+
+<hr>
+
+<!-- 콘텐츠 시작 { -->
+<div>
+    <div>
+   
+    <div>
